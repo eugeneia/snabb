e4ed51059e990645cf0a76f25d788380a2a73cc4
Max Rottenkolber <max@mr.gy>
2015-07-16 17:11:15 +0200

    [learing bridge] Process all ports on each push.

--- src/apps/bridge/learning.lua
+++ src/apps/bridge/learning.lua
@@ -76,7 +76,6 @@ function bridge:new (arg)
       end
    end
    o._nsrc_ports = #o._src_ports
-   o._port_index = 1
    -- Per-port MAC tables
    o._filters = {}
    for _, port in ipairs(o._src_ports) do
@@ -107,59 +106,56 @@ end
 -- understanding of the JIT compiler is needed to decide whether this
 -- is actually a good thing or not.  Empirical data suggests it is :)
 function bridge:push()
-   local src_port = self._src_ports[self._port_index]
-   local l_in = self.input[src_port]
-   while not empty(l_in) do
-      local cache = self._cache
-      local dst_ports = self._dst_ports
-      local p = cache.p
-      local dst = cache.dst
-      local src = cache.src
-      local dst_hash = nil
-      local filters = self._filters
-      p[0] = receive(l_in)
+   for port_index = 1, self._nsrc_ports do
+      local src_port = self._src_ports[port_index]
+      local l_in = self.input[src_port]
+      while not empty(l_in) do
+         local cache = self._cache
+         local dst_ports = self._dst_ports
+         local p = cache.p
+         local dst = cache.dst
+         local src = cache.src
+         local dst_hash = nil
+         local filters = self._filters
+         p[0] = receive(l_in)
 
-      dst[0] = packet.data(p[0])
-      src[0] = dst[0] + 6
+         dst[0] = packet.data(p[0])
+         src[0] = dst[0] + 6
 
-      -- Hash dst unless packet is multicast packet.
-      local is_mcast = ethernet:is_mcast(dst[0])
-      if not is_mcast then dst_hash = hash(dst[0]) end
+         -- Hash dst unless packet is multicast packet.
+         local is_mcast = ethernet:is_mcast(dst[0])
+         if not is_mcast then dst_hash = hash(dst[0]) end
 
-      -- Store the source MAC address in source port MAC table
-      filters[src_port]:insert(hash(src[0]), true)
+         -- Store the source MAC address in source port MAC table
+         filters[src_port]:insert(hash(src[0]), true)
 
-      local ports = dst_ports[src_port]
-      local copy = false
-      local j = 1
-      while ports[j] do
-         local dst_port = ports[j]
-         if is_mcast or filters[dst_port]:lookup(dst_hash) then
-            if not copy then
-               transmit(self.output[dst_port], p[0])
-               copy = true
-            else
-               transmit(self.output[dst_port], clone(p[0]))
-            end
-         end
-         j = j + 1
-      end
-      if not copy then
-         -- The source MAC address is unknown, flood the packet to
-         -- all ports
-         local output = self.output
-         transmit(output[ports[1]], p[0])
-         local j = 2
+         local ports = dst_ports[src_port]
+         local copy = false
+         local j = 1
          while ports[j] do
-            transmit(output[ports[j]], clone(p[0]))
+            local dst_port = ports[j]
+            if is_mcast or filters[dst_port]:lookup(dst_hash) then
+               if not copy then
+                  transmit(self.output[dst_port], p[0])
+                  copy = true
+               else
+                  transmit(self.output[dst_port], clone(p[0]))
+               end
+            end
             j = j + 1
          end
+         if not copy then
+            -- The source MAC address is unknown, flood the packet to
+            -- all ports
+            local output = self.output
+            transmit(output[ports[1]], p[0])
+            local j = 2
+            while ports[j] do
+               transmit(output[ports[j]], clone(p[0]))
+               j = j + 1
+            end
+         end
       end
-   end -- of while not empty(l_in)
-   if self._port_index == self._nsrc_ports then
-      self._port_index = 1
-   else
-      self._port_index = self._port_index + 1
    end
 end
 
