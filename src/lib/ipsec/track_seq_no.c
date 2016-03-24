36409cca65d7a9efeb5b73a454792d6e7ce46eaf
Max Rottenkolber <max@mr.gy>
2016-03-24 18:04:01 +0100

    lib.ipsec.esp: Replace `check_seq_no' with `track_seq_no' C function.

---
+++ src/lib/ipsec/track_seq_no.c
@@ -0,0 +1,11 @@
+#include <stdint.h>
+
+uint32_t track_seq_no (uint32_t seq_no, uint32_t Tl, uint32_t Th, uint32_t W) {
+  if (Tl >= W - 1) { // Case A
+    if (seq_no >= Tl - W + 1) return Th;
+    else                      return Th + 1;
+  } else {           // Case B
+    if (seq_no >= Tl - W + 1) return Th - 1;
+    else                      return Th;
+  }
+}
