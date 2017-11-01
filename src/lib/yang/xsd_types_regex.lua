e278865bb45d995dc79d34750301388751721a6b
Max Rottenkolber <max@mr.gy>
2017-11-01 11:43:08 +0100

    fixup regex

--- src/lib/yang/xsd_types_regex.lua
+++ src/lib/yang/xsd_types_regex.lua
@@ -7,50 +7,49 @@ module(..., package.seeall)
 local maxpc = require("lib.maxpc")
 local match, capture, combine = maxpc.import()
 
-function capture.token (x)
-   return capture.subseq(match.equal(x))
-end
-
 function capture.regExp ()
    return capture.unpack(
-      capture.seq(capture.branch(),
-                  combine.any(capture.otherBranch())),
+      capture.seq(capture.branch(), combine.any(capture.otherBranch())),
       function (branch, otherBranches)
          local branches = {branch}
          for _, branch in ipairs(otherBranches or {}) do
             table.insert(branches, branch)
          end
-         return branches
+         return {branches=branches}
       end
    )
 end
 
 function capture.branch ()
-   return combine.any(capture.piece())
+   return capture.transform(combine.any(capture.piece()),
+                            function (pieces) return {pieces=pieces} end)
 end
 
 function capture.otherBranch ()
    return capture.unpack(
       capture.seq(match.equal("|"), capture.branch()),
-      function (_, branch)
-         return branch
-      end
+      function (_, branch) return branch end
    )
 end
 
 function capture.piece ()
-   return capture.seq(capture.atom(), combine.maybe(capture.quantifier()))
+   return capture.unpack(
+      capture.seq(capture.atom(), combine.maybe(capture.quantifier())),
+      function (atom, quantifier)
+         return {atom=atom, quantifier=quantifier or nil}
+      end
+   )
 end
 
 function capture.quantifier ()
    return combine._or(
-      capture.token("?"),
-      capture.token("*"),
-      capture.token("+"),
-      capture.unpack(capture.seq(match.equal("{"),
-                                 capture.quanitity,
-                                 match.equal("}")),
-                     function (_, quantity, _) return quanitity end)
+      capture.subseq(match.equal("?")),
+      capture.subseq(match.equal("*")),
+      capture.subseq(match.equal("+")),
+      capture.unpack(
+         capture.seq(match.equal("{"), capture.quantity(), match.equal("}")),
+         function (_, quantity, _) return quantity end
+      )
    )
 end
 
@@ -63,9 +62,27 @@ function match.digit (s)
 end
 
 function capture.quantity ()
-   return capture.seq(
-      capture.quantExact(),
-      combine.maybe(capture.quantMax())
+   return combine._or(
+      capture.quantRange(),
+      capture.quantMin(),
+      capture.transform(capture.quantExact(),
+                        function (n) return {exactly=n} end)
+   )
+end
+
+function capture.quantRange ()
+   return capture.unpack(
+      capture.seq(capture.quantExact(),
+                  match.equal(","),
+                  capture.quantExact()),
+      function (min, _, max) return {min=min, max=max} end
+   )
+end
+
+function capture.quantMin ()
+   return capture.unpack(
+      capture.seq(capture.quantExact(), match.equal(",")),
+      function (min, _) return {min=min} end
    )
 end
 
@@ -73,13 +90,6 @@ function capture.quantExact ()
    return capture.transform(
       capture.subseq(combine.some(match.digit())),
       tonumber
-   )
-end
-
-function capture.quantMax ()
-   return capture.unpack(
-      capture.seq(match.equal(","), capture.quantExact()),
-      function (_, max) return max end
    )
 end
 
@@ -92,10 +102,11 @@ function capture.atom ()
 end
 
 local regExp_parser -- forward definition
+local function regExp_binding (s) return regExp_parser(s) end
 
 function capture.subExp ()
    return capture.unpack(
-      capture.seq(match.equal('('), regExp_parser, match.equal(')')),
+      capture.seq(match.equal('('), regExp_binding, match.equal(')')),
       function (_, expression, _) return expression end
    )
 end
@@ -132,30 +143,49 @@ function capture.charClassExpr ()
    )
 end
 
-local charClassExpr_parser -- forward declaration
-
 function capture.charGroup ()
-   local subtract = capture.unpack(
-      capture.seq(match.equal("-"), charClassExpr_parser),
-      function (_, charClassExpr, _) return charClassExpr end
+   return capture.unpack(
+      capture.seq(
+         combine._or(capture.negCharGroup(), capture.posCharGroup()),
+         combine.maybe(capture.charClassSubtraction())
+      ),
+      function (group, subtract)
+         return {class=group, subtract=subtract or nil}
+      end
    )
-   return capture.seq(
-      combine._or(capture.posCharGroup(), capture.negCharGroup()),
-      combine.maybe(subtract)
+end
+
+local charClassExpr_parser -- forward declaration
+local function charClassExpr_binding (s)
+   return charClassExpr_parser(s)
+end
+
+function capture.charClassSubtraction ()
+   return capture.unpack(
+      capture.seq(match.equal("-"), charClassExpr_binding),
+      function (_, charClassExpr, _) return charClassExpr end
    )
 end
 
 function capture.posCharGroup ()
-   return combine.some(capture.charGroupPart())
+   return capture.transform(
+      combine.some(capture.charGroupPart()),
+      function (parts) return {include=parts} end
+   )
 end
 
 function capture.negCharGroup ()
-   return capture.seq(capture.token("^"), capture.posCharGroup())
+   return capture.unpack(
+      capture.seq(match.equal("^"), capture.posCharGroup()),
+      function (_, group) return {exclude=group.include} end
+   )
 end
 
 function capture.charGroupPart ()
    return combine._or(
-      capture.singleChar(), capture.charRange(), capture.charClassEsc()
+      capture.charClassEsc(),
+      capture.charRange(),
+      capture.singleChar()
    )
 end
 
@@ -164,8 +194,10 @@ function capture.singleChar ()
 end
 
 function capture.charRange ()
-   return capture.seq(
-      capture.singleChar(), match.equal("-"), capture.singleChar()
+   local rangeChar = combine.diff(capture.singleChar(), match.equal("-"))
+   return capture.unpack(
+      capture.seq(rangeChar, match.equal("-"), rangeChar),
+      function (from, _, to) return {range={from,to}} end
    )
 end
 
@@ -173,7 +205,11 @@ function capture.singleCharNoEsc ()
    local function is_singleCharNoEsc (s)
       return not ("[]"):find(s, 1, true)
    end
-   return capture.subseq(match.satisfies(is_singleCharNoEsc))
+   return combine.diff(
+      capture.subseq(match.satisfies(is_singleCharNoEsc)),
+      -- don’t match the "-" leading a character class subtraction
+      match.seq(match.equal("-"), match.equal("["))
+   )
 end
 
 function capture.charClassEsc ()
@@ -186,9 +222,12 @@ function capture.SingleCharEsc ()
    local function is_SingleCharEsc (s)
       return ("nrt\\|.?*+(){}-[]^"):find(s, 1, true)
    end
-   return capture.seq(
-      capture.token("\\"),
-      capture.subseq(match.satisfies(is_SingleCharEsc))
+   return capture.unpack(
+      capture.seq(
+         match.equal("\\"),
+         capture.subseq(match.satisfies(is_SingleCharEsc))
+      ),
+      function (_, char) return {escape=char} end
    )
 end
 
@@ -198,19 +237,26 @@ function capture.MultiCharEsc ()
    local function is_multiCharEsc (s)
       return ("sSiIcCdDwW"):find(s, 1, true)
    end
-   return capture.seq(
-      capture.token("\\"),
-      capture.subseq(match.satisfies(is_multiCharEsc))
+   return capture.unpack(
+      capture.seq(
+         match.equal("\\"),
+         capture.subseq(match.satisfies(is_multiCharEsc))
+      ),
+      function (_, char) return {escape=char} end
    )
 end
 
 function capture.WildcardEsc ()
-   return capture.token(".")
+   return capture.transform(
+      match.equal("."),
+      function (_) return {escape="."} end
+   )
 end
 
 regExp_parser = capture.regExp()
 charClassExpr_parser = capture.charClassExpr()
 
 function parse (expr)
-   return maxpc.parse(expr, regExp_parser)
+   local result, success, is_eof = maxpc.parse(expr, regExp_parser)
+   return (success and is_eof and result) or nil
 end
