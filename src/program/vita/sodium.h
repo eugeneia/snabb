65f78f5b85e093aeb7f91403ca3c3f73d128848b
Max Rottenkolber <max@mr.gy>
2017-12-10 22:51:07 +0100

    wip

--- src/program/vita/sodium.h
+++ src/program/vita/sodium.h
@@ -1,30 +1,53 @@
-// sodium/core.h
+/* Use of this source code is governed by the Apache 2.0 license; see COPYING. */
+
+// core.h
 int sodium_init(void);
 
-// sodium/randombytes.h:
+// randombytes.h:
 void randombytes_buf(void * const buf, const size_t size);
 
-// sodium/crypto_aead_xchacha20poly1305.h
+// crypto_auth_hmacsha512256.h
 enum {
-  crypto_aead_xchacha20poly1305_ietf_NPUBBYTES = 24U,
-  crypto_aead_xchacha20poly1305_ietf_KEYBYTES = 32U,
-  crypto_aead_xchacha20poly1305_ietf_ABYTES = 16U
+  crypto_auth_hmacsha512256_BYTES = 32U,
+  crypto_auth_hmacsha512256_KEYBYTES = 32U
 };
-int crypto_aead_xchacha20poly1305_ietf_encrypt(unsigned char *c,
-                                               unsigned long long *clen_p,
-                                               const unsigned char *m,
-                                               unsigned long long mlen,
-                                               const unsigned char *ad,
-                                               unsigned long long adlen,
-                                               const unsigned char *nsec,
-                                               const unsigned char *npub,
-                                               const unsigned char *k);
-int crypto_aead_xchacha20poly1305_ietf_decrypt(unsigned char *m,
-                                               unsigned long long *mlen_p,
-                                               unsigned char *nsec,
-                                               const unsigned char *c,
-                                               unsigned long long clen,
-                                               const unsigned char *ad,
-                                               unsigned long long adlen,
-                                               const unsigned char *npub,
-                                               const unsigned char *k);
+int crypto_auth_hmacsha512256(unsigned char *out, const unsigned char *in,
+                              unsigned long long inlen,const unsigned char *k);
+int crypto_auth_hmacsha512256_verify(const unsigned char *h,
+                                     const unsigned char *in,
+                                     unsigned long long inlen,
+                                     const unsigned char *k);
+
+// crypto_scalarmult_curve25519.h
+enum {
+  crypto_scalarmult_curve25519_BYTES = 32U,
+  crypto_scalarmult_curve25519_SCALARBYTES = 32U
+};
+int crypto_scalarmult_curve25519_base(unsigned char *q,
+                                      const unsigned char *n);
+int crypto_scalarmult_curve25519(unsigned char *q,
+                                 const unsigned char *n,
+                                 const unsigned char *p)
+
+// crypto_generichash_blake2b.h
+enum {
+  crypto_generichash_blake2b_BYTES = 32U,
+  crypto_generichash_blake2b_KEYBYTES = 32U
+};
+struct CRYPTO_ALIGN(64) crypto_generichash_blake2b_state {
+    uint64_t h[8];
+    uint64_t t[2];
+    uint64_t f[2];
+    uint8_t  buf[2 * 128];
+    size_t   buflen;
+    uint8_t  last_node;
+} __attribute__ ((aligned(64)));
+int crypto_generichash_blake2b_init(crypto_generichash_blake2b_state *state,
+                                    const unsigned char *key,
+                                    const size_t keylen, const size_t outlen);
+int crypto_generichash_blake2b_update(crypto_generichash_blake2b_state *state,
+                                      const unsigned char *in,
+                                      unsigned long long inlen);
+int crypto_generichash_blake2b_final(crypto_generichash_blake2b_state *state,
+                                     unsigned char *out,
+                                     const size_t outlen);
